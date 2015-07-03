becb9043165481f38a763621b91ae11ea715ef2b
j976573957 <MacOSX@MacdeMacBook-Pro.local>
2015-07-03 22:58:35 +0800

    Initial Commit

---
+++ myMusicDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myMusicDemo
+//
+//  Created by Mac OS X on 7/3/15.
+//  Copyright (c) 2015 Mac OS X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myMusicDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myMusicDemo
+//
+//  Created by Mac OS X on 7/3/15.
+//  Copyright (c) 2015 Mac OS X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
